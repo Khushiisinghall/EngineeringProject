aa8af7514ccc20893eca7f1ab7bf88803eaac418
Nam Nguyen <87677793+NamNguyen900@users.noreply.github.com>
2023-05-08 02:36:39 +0200

    Add Login Page

--- Frontend/src/app/vital-tracker/header/header.component.ts
+++ Frontend/src/app/vital-tracker/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, DoCheck } from '@angular/core';
 import { LiveService } from 'src/app/services/live.service';
 import { Subscription } from 'rxjs';
 import { Router } from '@angular/router';
@@ -8,21 +8,43 @@ import { Router } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements DoCheck{
 
   title: string = 'Vital Tracker'
   showAddTask: boolean;
   subscription: Subscription;
+  //Login page
+  isadmin=false;
+  isMenuVisible=false;
 
   constructor(@Inject(LiveService) private uiService:LiveService,
             private router: Router) {
     this.subscription = this.uiService
         .onToggle()
         .subscribe((value) => this.showAddTask = value);
+    //Login page
+    let role=sessionStorage.getItem('role');
+      if(role=='admin'){
+        this.isadmin=true;
+      }
   }
 
   ngOnInit(): void {}
+  ngDoCheck(): void {
+    let currentroute = this.router.url;
+    let role=sessionStorage.getItem('role');
+    if (currentroute == '/login' || currentroute == '/register') {
+      this.isMenuVisible = false
+    } else {
+      this.isMenuVisible = true
+    }
 
+    if (role == 'admin') {
+      this.isadmin = true;
+    }else{
+      this.isadmin = false;
+    }
+  }
 
   toggleAddTask() {
     this.uiService.toggleAddTask();
