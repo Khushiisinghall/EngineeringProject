e12ba7d76cc5880a4d9f9346793524c569bb77d0
Nam Nguyen <87677793+NamNguyen900@users.noreply.github.com>
2023-05-09 22:11:58 +0200

    Styling in Frontend

--- Frontend/src/app/vital-tracker/service/auth.service.ts
+++ Frontend/src/app/vital-tracker/service/auth.service.ts
@@ -14,16 +14,16 @@ export class AuthService {
   hardwareapiurl='';
 
   RegisterUser(inputdata:any){
-    return this.http.post(this.apiurl,inputdata)
+    return this.http.post(this.mongourl,inputdata)
   }
   GetUserbyCode(id:any){
-    return this.http.get(this.apiurl+'/'+id);
+    return this.http.get(this.mongourl+'/'+id);
   }
   Getall(){
-    return this.http.get(this.apiurl);
+    return this.http.get(this.mongourl);
   }
   updateuser(id:any,inputdata:any){
-    return this.http.put(this.apiurl+'/'+id,inputdata);
+    return this.http.put(this.mongourl+'/'+id,inputdata);
   }
   CallHardware(userid:any){
      return this.http.post(this.hardwareapiurl,userid);
